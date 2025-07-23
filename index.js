73fc51a04d5c644ff86abd111c55320a52077755
unknown <cnl.ipek@gmail.com>
2025-07-23 15:11:55 +0300

    json dosyası oluştur veri ekleme ve jsondan veri çekme

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-require("./batman");
-require("./superman");
+// require("./batman");
+// require("./superman");
 
-// iife
+// // iife
 
-(function () {})(); // bunda aynı isimden birden fazla yazsak bile qsorun çıkartmıyor buna iife diyoruz sanırım!
+// (function () {})(); // bunda aynı isimden birden fazla yazsak bile qsorun çıkartmıyor buna iife diyoruz sanırım!
 
-(function () {
-  const superhero = "Batman";
-  console.log(superhero);
-})();
+// (function () {
+//   const superhero = "Batman";
+//   console.log(superhero);
+// })();
 
-(function () {
-  const superhero = "Superman";
-  console.log(superhero);
-})();
+// (function () {
+//   const superhero = "Superman";
+//   console.log(superhero);
+// })();
+
+const data = require("./data.json");
+console.log(data);

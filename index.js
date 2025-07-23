ed256e867f9708bdc65e2d004a91f7e4daf50266
unknown <cnl.ipek@gmail.com>
2025-07-23 14:48:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+require("./batman");
+require("./superman");
+
+// iife
+
+(function () {})(); // bunda aynı isimden birden fazla yazsak bile qsorun çıkartmıyor buna iife diyoruz sanırım!
---
+++ topla.js
@@ -0,0 +1,16 @@
+const toplamaYap = (a, b) => {
+  return a + b;
+};
+
+const bolmeYap = (a, b) => {
+  return a / b;
+};
+const carpmaYap = (a, b) => {
+  return a * b;
+};
+
+module.exports = {
+  toplamaYap,
+  bolmeYap,
+  carpmaYap,
+};

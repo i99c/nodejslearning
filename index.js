a6db3fcc5da15f0ceff054a87610cdd8d83753ac
unknown <cnl.ipek@gmail.com>
2025-07-23 15:01:44 +0300

    iife

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ require("./superman");
 // iife
 
 (function () {})(); // bunda aynı isimden birden fazla yazsak bile qsorun çıkartmıyor buna iife diyoruz sanırım!
+
+(function () {
+  const superhero = "Batman";
+  console.log(superhero);
+})();
+
+(function () {
+  const superhero = "Superman";
+  console.log(superhero);
+})();
